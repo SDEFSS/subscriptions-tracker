a30cba4ac6469c89bef77d965ac8464f0b6475d9
eghunt <eghunt@ttu.ee>
2020-07-23 15:40:00 +0300

    Made subscription code cleaner.

--- frontend/src/components/subscription.jsx
+++ frontend/src/components/subscription.jsx
@@ -3,14 +3,16 @@ import React, {Component} from 'react';
 class Subscription extends Component {
 
     render() {
+        const { onDelete } = this.props;
+        const { id, imageUrl, value } = this.props.subscription;
         return (
             <div className="container m-2">
                 <div class="row">
-                    <span className="m-2 col-sm-2">{this.props.subscription.id}</span>
-                    <img src={this.props.subscription.imageUrl} alt=""/>
-                    <span className="badge m-2 col-sm-1">{this.props.subscription.value}</span>
+                    <span className="m-2 col-sm-2">{id}</span>
+                    <img src={imageUrl} alt=""/>
+                    <span className="badge m-2 col-sm-1">{value}</span>
                     <button
-                        onClick={() => this.props.onDelete(this.props.subscription.id)}
+                        onClick={() => onDelete(id)}
                         className="col-sm-1 btn btn-danger btn-sm m-2">
                         delete
                     </button>
