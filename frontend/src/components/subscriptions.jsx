73722ce4daa4804d9853ccb77dc6654b55b20536
eghunt <eghunt@ttu.ee>
2020-07-23 21:31:54 +0300

    Placeholder new button.

--- frontend/src/components/subscriptions.jsx
+++ frontend/src/components/subscriptions.jsx
@@ -10,6 +10,10 @@ class Subscriptions extends Component {
                 <div className=" ml-4 mb-5 mt-4">
                     <h2><b> Manage subscriptions </b></h2>
                 </div>
+                <button
+                    className="btn btn-sm float-lg-right btn-success mr-5 pr-3 pl-3">
+                    <h5><b>new</b></h5>
+                </button>
                 <div className="container mt-5">
                     <div className="row mb-5 mt-5">
                         <h5 className="col-sm-4">subscription name</h5>
