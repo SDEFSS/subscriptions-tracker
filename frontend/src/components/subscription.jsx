058b2607a8abdfa0dd86aa66edf5fd483f46e8fd
eghunt <eghunt@ttu.ee>
2020-07-23 16:09:15 +0300

    Subscriptions show the subscription picture before the subscription name

--- frontend/src/components/subscription.jsx
+++ frontend/src/components/subscription.jsx
@@ -8,8 +8,8 @@ class Subscription extends Component {
         return (
             <div className="container m-2">
                 <div class="row">
-                    <span className="m-2 col-sm-2">{id}</span>
                     <img src={imageUrl} alt=""/>
+                    <span className="m-2 col-sm-2">{id}</span>
                     <span className="badge m-2 col-sm-1">{value}</span>
                     <button
                         onClick={() => onDelete(id)}
